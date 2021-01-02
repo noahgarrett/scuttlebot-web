1ebc98ab40a1014e3cae7224a6c84763d6266c57
Daniel Crumpler <dancrumpy@gmail.com>
2021-01-02 13:13:50 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>New page</h1>
-      <h2>Author: Noah Garrett and Daniel Crumpler</h2>
-      <h3>Scuttle Bot</h3>
+      <h1>Work in Progress</h1>
     </div>
   );
 }
