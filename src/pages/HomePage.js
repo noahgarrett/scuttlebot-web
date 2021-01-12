ab66d40a155e69e8b8b221de80192f81de47a1d2
Noah Garrett <67662284+xsychgames@users.noreply.github.com>
2021-01-11 23:47:21 -0500

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,12 @@ function HomePage() {
       <div>
         <h1 className="title">ScuttleBot</h1>
         <div className="images">
+          <div className="topgg">
+            <h3 className='h_topgg'>Verified on Top.gg</h3>
+            <a href="https://top.gg/bot/791336194230845490" className='link'>
+              Visit the ScuttleBot page
+            </a>
+          </div>
           <p>Help Command</p>
           <img
             src="https://cdn.discordapp.com/attachments/787493493927968790/798404235845828618/help.png"
@@ -19,12 +25,37 @@ function HomePage() {
             alt="Analytics Command"
           ></img>
 
-          <p>Analytics Command</p>
+          <p>Champion Command</p>
           <img
-            src="https://cdn.discordapp.com/attachments/787493493927968790/798404219644280842/analytics.png"
+            src="https://cdn.discordapp.com/attachments/787493493927968790/798404225385365564/champion.png"
             alt="Analytics Command"
           ></img>
+
+          <p>Mastery Command</p>
+          <img
+            src="https://cdn.discordapp.com/attachments/787493493927968790/798404240980312074/mastery.png"
+            alt="Mastery Command"
+          ></img>
+
+          <p>Random Champion Command</p>
+          <img
+            src="https://cdn.discordapp.com/attachments/787493493927968790/798404245967339520/random.png"
+            alt="Random Champion Command"
+          ></img>
+
+          <p>Free Rotation Command</p>
+          <img
+            src="https://cdn.discordapp.com/attachments/787493493927968790/798404230868238397/free2play.png"
+            alt="Free Rotation Command"
+          ></img>
+
+          <p>Tier List Command</p>
+          <img
+            src="https://cdn.discordapp.com/attachments/787493493927968790/798404215507648533/tierlist.png"
+            alt="Tier List Command"
+          ></img>
         </div>
+        <h3 className='info'>Website still in development...</h3>
 
         {/* <div className="community_discord_link">
           <a
